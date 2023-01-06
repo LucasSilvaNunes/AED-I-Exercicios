cfda04c15af75e4a166415c81fdd6ff9b6e87d0c
Lucas Nunes <lucassz_rj@hotmail.com>
2023-01-06 19:23:28 -0300

    Update alocDinamicaExercicio.c

--- alocDinamicaExercicio.c
+++ alocDinamicaExercicio.c
@@ -10,27 +10,49 @@ typedef struct{
 int main(){
     produto* p;
     int resp, cont=0;
+    float maiorp = 0;
+    
     p = (produto*) malloc(10*sizeof(produto));
     
-    do{
-        printf("Código: ");
-        scanf("%d", &p[cont].cod);
-        getchar();
+    if(p != NULL){
+        do{
+            printf("Código: ");
+            scanf("%d", &p[cont].cod);
+            getchar();
+            
+            printf("Nome: ");
+            fgets(p[cont].nome, 50, stdin);
+            
+            printf("Estoque: ");
+            scanf("%d", &p[cont].estq);
+            
+            printf("Preço:");
+            scanf("%f", &p[cont].preco);
+            
+            cont++;
+            
+            printf("Deseja continuar cadastrando? 1-sim / 0-não\n");
+            scanf("%d", &resp);
+        }while(resp!=0);
+    }else{
+        printf("Memoria insuficiente!");
+        return 1;
+    }
+    
+    for(int i=0; i<cont; i++){
+        if(p[i].preco > maiorp){
+            maiorp = p[i].preco;
+        }
+    }
+    
+    for(int i=0; i<cont; i++){
+        if(p[i].preco == maiorp){
+            printf("\nCódigo: %d\nNome: %s\nEstoque: %d\nPreço: %f", p[i].cod, p[i].nome, p[i].estq, p[i].preco);
+        }
+    }
+    
         
-        printf("Nome: ");
-        fgets(p[cont].nome, 50, stdin);
-        
-        printf("Estoque: ");
-        scanf("%d", &p[cont].estq);
-        
-        printf("Preço:");
-        scanf("%f", &p[cont].preco);
-        
-        cont++;
-        
-        printf("Deseja continuar cadastrando? 1-sim / 0-não\n");
-        scanf("%d", &resp);
-    }while(resp!=0);
+    
     
     return 0;
 }
